ef7f322735f90b47aef18f49a0b8a84ea8145304
Donovan Seulin <nel640@hotmail.fr>
2024-01-03 09:11:17 +0100

    "ajout des question a l'utilisateur plus boucle"

--- src/SimulateurArriver.java
+++ src/SimulateurArriver.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
 
 public class SimulateurArriver 
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("ca marche");
+		Scanner scan = new Scanner(System.in);
+		Boolean continBool = true;
+		
+		while(continBool)
+		{
+			System.out.println("combien de chevaux sont dans la course ? (12 a 20 chevaux) : ");
+			int horsesNumber = scan.nextInt();
+			System.out.println("quel est le type de couse ? (tierce, quarte, quinte) : ");
+			String runType = scan.next();
+			
+			System.out.println("voulez vous rejouez ? : ");
+			String continueAnswerString = scan.next();
+			
+			if(continueAnswerString.equals("O"))
+			{
+				continBool = true;
+			}
+			else 
+			{
+				continBool = false;
+				System.out.println(" a bientot ");
+			}
+		}
+		scan.close();
 	}
 }
